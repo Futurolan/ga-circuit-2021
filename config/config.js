885741b8d7ffed3ea97721360b893cdb369a33ff
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2020-12-12 18:53:41 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 const config = {
-  title: 'Gamers Assembly : Halloween Edition 2020',
-  logo: '/static/img/logo-orange-100x100.png',
-  description: 'L\'édition 2020 de la Gamers Assembly Halloween Edition aura lieu du ?? au ?? ???????? à ???',
-  metaImagePath: '/static/img/logo-orange-200x200.png',
-  gaTrackingId: 'UA-128777022-7',
+  title: 'Gamers Assembly : Circuit 2021',
+  logo: '/static/img/logo.png',
+  description: 'Gamers Assembly 2021 : un circuit inédit !',
+  metaImagePath: '/static/img/logo.png',
+  gaTrackingId: 'UA-128777022-14',
   mainPartner: {
-    logo: '/static/img/logo_st_benoit.jpg',
-    url: 'http://www.ville-saint-benoit.fr/'
+    logo: '/static/img/logo_gp.png',
+    url: 'https://www.grandpoitiers.fr/'
   },
   home: {
     banner: '/static/img/banner.png',
@@ -26,7 +26,7 @@ const config = {
   },
   news: {
     title: 'Actualités',
-    description: 'Retrouvez toutes les actualités de la Gamers Assembly Halloween Edition 2020',
+    description: 'Retrouvez toutes les actualités du circuit Gamers Assembly 2021',
     link: '/news'
   },
   partners: {
@@ -36,8 +36,8 @@ const config = {
   },
   tournaments: {
     title: 'Tournois',
-    navTitle: '<img src="/static/img/tournoisga.png" style="max-height:none">',
-    description: 'Tous les tournois de la Gamers Assembly 2020',
+    // navTitle: '<img src="/static/img/tournoisga.png" style="max-height:none">',
+    description: 'Tous les tournois de la prochaine étape du circuit Gamers Assembly 2021',
     // notification: '<div class="columns is-vcentered has-text-centered"><div class="column is-2"><img src="/static/img/logo_ga_oes.png"></div><div class="column">Il ne peut pas y avoir d\'Occitanie Esports sans sa Gamers Assembly et son lot de tournois !<br>Voici donc tous les tournois aussi esports et fun les uns que les autres mais surtout le plus important : des tournois GA certified.</div></div>',
     link: '/tournois'
   },
@@ -58,7 +58,7 @@ const config = {
   },
   tickets: {
     title: 'Billetterie',
-    description: 'Achetez vos places pour la Gamers Assembly Halloween Edition 2020',
+    description: 'Achetez vos places pour le circuit Gamers Assembly 2021',
     link: '/billetterie',
     // notification: '<strong>ATTENTION !</strong><br />Jusqu\'au 11 mars, profitez d\'une réduction de 10 € par place joueur avec le code de réduction <strong>#GA2020</strong><br />  (NE PAS OUBLIER dès le début de l\'inscription, à l\'étape 1)',
     pass: {
